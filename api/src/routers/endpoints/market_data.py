8d597bed7584e0924a1574d44af87b5e3f43791e
samuelcsouza <samuel_dacruz@hotmail.com>
2022-06-30 08:43:29 -0300

    feat: used steam service

--- api/src/routers/endpoints/market_data.py
+++ api/src/routers/endpoints/market_data.py
@@ -1,22 +1,24 @@
-from src.services.crawler import Crawler
-from fastapi import APIRouter, Query
+from fastapi import APIRouter, Query, HTTPException
+from src.services.external.steam import SteamService
 
 router = APIRouter()
 
+steam_service = SteamService(
+    steam_url_marketplace="https://steamcommunity.com/market/listings/730/"
+)
+
+
 @router.get("/")
 async def get_marketplace_data(
-    item = Query(None),
-    interval = Query(None)
+    item=Query(None)
 ):
 
-    fields = {
-        'item': item,
-        'interval': interval
-    }
-
     try:
-        result = await Crawler.get_marketplace_data(fields)
+        result = steam_service.get_item_marketplace_values(item)
     except Exception as exc:
-        return {'Error!': str(exc)}
+        raise HTTPException(
+            status_code=400,
+            detail=str(exc)
+        )
 
     return result
