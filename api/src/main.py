0ec7bf2922a5687f543d3e483089bcd5cc76218e
samuelcsouza <samuel_dacruz@hotmail.com>
2022-07-07 13:25:55 -0300

    feat: handle with exceptions

--- api/src/main.py
+++ api/src/main.py
@@ -1,8 +1,36 @@
 import importlib
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from starlette.exceptions import HTTPException
+from src.utils import api_errors
 
 app = FastAPI()
+
+
+@app.exception_handler(HTTPException)
+async def api_exception_handler(request: Request, exception: HTTPException) -> JSONResponse:
+
+    error_status_code, error_detail = exception.status_code, exception.detail
+
+    if isinstance(error_detail, str):
+        if error_status_code == api_errors.ErrorRouteNotFound.status_code:
+            error_detail = api_errors.ErrorRouteNotFound.error
+        elif error_status_code == api_errors.ErrorMethodNotAllowed.status_code:
+            error_detail = api_errors.ErrorMethodNotAllowed.error
+
+    return JSONResponse(
+        status_code=error_status_code,
+        content={"error": error_detail},
+    )
+
+
+@app.exception_handler(api_errors.BusinessException)
+async def business_exception_handler(request: Request, exception: api_errors.BusinessException) -> JSONResponse:
+    try:
+        api_errors.raise_error_response(exception, str(exception) or None)
+    except HTTPException as e:
+        return await api_exception_handler(request, e)
 
 api_module = importlib.import_module("src.routers.api")
 
